b2c9fff4b7020634608d4c05cdb722604f372e8d
cva44 <shivakrishna.vosukula@gmail.com>
2026-01-20 20:29:14 +0530

    s

--- provider.tf
+++ provider.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.84.0"
+      version = "6.28.0"
     }
   }
 
   backend "s3" {
-    bucket = "cva-tf-remote-state-dev1"
+    bucket = "cva-tf-remote-state"
     key    = "expense-cicd" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
     region = "us-east-1"
-    dynamodb_table = "cva-tf-remote-state-dev1"
+    dynamodb_table = "cva-tf-remote-state"
   }
 }
 
