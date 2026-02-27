3200b357623e0554e29fea4358acf05ac2271cd6
cva44 <shivakrishna.vosukula@gmail.com>
2026-02-27 11:41:32 +0530

    key

--- data.tf
+++ data.tf
@@ -15,13 +15,29 @@
 #        values = ["hvm"]
 #    }
 #}
+#3data "aws_ami" "ami_info" {
+#  most_recent = true
+#  owners      = ["309956199498"] # Red Hat official
+
+#  filter {
+#    name   = "name"
+#    values = ["RHEL-9.*_HVM-*x86_64*"]
+#  }
+#}
+#Reasons:
+#RHEL requires subscription handling
+#Some third-party repos behave differently
+#Extra entitlement checks
+#Enterprise restrictions
+#For CI/CD lab practice, this is unnecessary complexity.
+
 
 data "aws_ami" "ami_info" {
   most_recent = true
-  owners      = ["309956199498"] # Red Hat official
+  owners      = ["amazon"]
 
   filter {
     name   = "name"
-    values = ["RHEL-9.*_HVM-*x86_64*"]
+    values = ["al2023-ami-*-x86_64"]
   }
 }
