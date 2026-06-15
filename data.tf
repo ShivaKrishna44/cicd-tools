fcdeabcf062c292ee07f4eb39f9fba4ad399b2d9
cva44 <shivakrishna.vosukula@gmail.com>
2026-06-15 22:54:43 +0530

    jenkins fix

--- data.tf
+++ data.tf
@@ -1,5 +1,4 @@
 data "aws_ami" "ami_info" {
-<<<<<<< HEAD
   most_recent = true
   owners      = ["309956199498"] # Official Amazon Red Hat Account ID
 
@@ -18,24 +17,4 @@ data "aws_ami" "ami_info" {
     values = ["hvm"]
   }
 }
-=======
 
-    most_recent = true
-    owners = ["973714476881"]
-
-    filter {
-        name   = "name"
-        values = ["RHEL-9-DevOps-Practice"]
-    }
-
-    filter {
-        name   = "root-device-type"
-        values = ["ebs"]
-    }
-
-    filter {
-        name   = "virtualization-type"
-        values = ["hvm"]
-    }
-}
->>>>>>> b32708653183fc9134b4816d0e56892467f73adc
