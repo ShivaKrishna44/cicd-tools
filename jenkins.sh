f6ff22a34b520faaf04d8817bfffd32dd6893d1a
cva44 <shivakrishna.vosukula@gmail.com>
2026-06-16 12:23:32 +0530

    chore: migrate environment architecture to amazon linux 2023

--- jenkins.sh
+++ jenkins.sh
@@ -13,19 +13,24 @@ if lsblk | grep -q nvme0n1p1; then
 fi
 
 echo "===== Installing Packages ====="
+dnf clean all
 dnf update -y
-dnf install -y git java-17-amazon-corretto wget
+#dnf install -y git java-17-amazon-corretto wget
+dnf install -y git wget fontconfig java-21-amazon-corretto
 
 echo "===== Configuring Jenkins Repository (AL2023 Compatible) ====="
 rm -f /etc/yum.repos.d/jenkins.repo
 
 cat <<EOF > /etc/yum.repos.d/jenkins.repo
 [jenkins]
-name=Jenkins
-baseurl=https://pkg.jenkins.io/rpm-stable
-enabled=1
+name=Jenkins-stable
+baseurl=https://jenkins.io
+#enabled=1
 gpgcheck=0
 EOF
+
+# Import the correct verification key file path
+rpm --import https://jenkins.iojenkins.io-2023.key
 
 dnf clean all
 dnf makecache
